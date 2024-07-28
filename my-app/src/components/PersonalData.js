beec5217d081862de75b72bf3a95adc43f9fdfc7
Coding With Shawn <shawnray5699@gmail.com>
2024-07-28 11:22:52 -0500

    map+boxes

--- my-app/src/components/PersonalData.js
+++ my-app/src/components/PersonalData.js
@@ -4,6 +4,9 @@ import './PersonalData.css';
 const PersonalData = () => {
   return (
     <div id="personal-data">
+      <div className="glowing-background"></div>
+      <div className="world-map"></div>
+      <div className="radial-lines"></div>
       <div className="personal-data-container">
         <div className="left-panel">
           <div className="confidential-data">
@@ -20,23 +23,19 @@ const PersonalData = () => {
           <div className="fingerprint"></div>
         </div>
         <div className="center-panel">
-          <div className="profile-picture">
-            <div className="ascii-person">[Identity Person]</div>
-          </div>
+          <div className="profile-picture"></div>
+          <div className="ascii-person">[Identity Person]</div>
         </div>
         <div className="right-panel">
-          <h2>Personal Data</h2>
-          <ul>
-            <li>Name: </li>
-            <li>Home Address: </li>
-            <li>Business Address: </li>
-            <li>Identity Card No: </li>
-            <li>Passport No: </li>
-            <li>Driving License: </li>
-            <li>Income Tax No: </li>
-            <li>Car Registration: </li>
-            <li>Other: </li>
-          </ul>
+          <div className="data-item">Name: John Doe</div>
+          <div className="data-item">Home Address: 123 Main St</div>
+          <div className="data-item">Business Address: 456 Business Rd</div>
+          <div className="data-item">Identity Card No: 123456789</div>
+          <div className="data-item">Passport No: X1234567</div>
+          <div className="data-item">Driving License: D1234567</div>
+          <div className="data-item">Income Tax No: IT123456</div>
+          <div className="data-item">Car Registration: CR123456</div>
+          <div className="data-item">Other: Additional Info</div>
         </div>
       </div>
     </div>
